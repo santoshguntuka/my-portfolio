55dafb8e59a8cf00252bdea5477812d6a895fca6
santoshguntuka <santoshguntuka7@gmail.com>
2024-09-03 19:50:11 -0700

    saved changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React, { useState } from 'react'; // Import useState from React
->>>>>>> 7d08da0 (Initial commit)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-<<<<<<< HEAD
+import Footer from './components/Footer';
 import { DarkModeProvider, useDarkMode } from './DarkModeContext';
 import './styles/App.css';
 
 function AppContent() {
-  const { isDarkMode } = useDarkMode();
+  const { isDarkMode, toggleDarkMode } = useDarkMode(); // Access dark mode state and toggle function
 
   return (
     <div className={`app-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <Navbar />
+      <div className="switch-container">
+        <label className="switch">
+          <input type="checkbox" onChange={toggleDarkMode} checked={isDarkMode} />
+          <span className="slider round"></span>
+        </label>
+        <span>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
+      </div>
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -28,41 +31,8 @@ function AppContent() {
           <Route path="*" element={<Home />} /> {/* Fallback to Home */}
         </Routes>
       </div>
+      <Footer />
     </div>
-=======
-import Footer from './components/Footer';
-import './styles/App.css';
-
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true); // Now useState is properly defined
-
-  const toggleDarkMode = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
-
-  return (
-    <Router>
-      <div className={`app-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-        <Navbar />
-        <div className="switch-container">
-          <label className="switch">
-            <input type="checkbox" onChange={toggleDarkMode} checked={isDarkMode} />
-            <span className="slider round"></span>
-          </label>
-          <span>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
-        </div>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
->>>>>>> 7d08da0 (Initial commit)
   );
 }
 
