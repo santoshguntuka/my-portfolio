398bf0702167e81a6f94ec304ab6413ac8c6fb19
santoshguntuka <santoshguntuka7@gmail.com>
2024-09-04 12:53:26 -0700

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
