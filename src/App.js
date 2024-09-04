a1e2282164821389a3a2fdf74e3866b0e6b4379b
santoshguntuka <santoshguntuka7@gmail.com>
2024-09-03 20:06:20 -0700

    Your commit message

--- src/App.js
+++ src/App.js
@@ -10,25 +10,18 @@ import { DarkModeProvider, useDarkMode } from './DarkModeContext';
 import './styles/App.css';
 
 function AppContent() {
-  const { isDarkMode, toggleDarkMode } = useDarkMode(); // Access dark mode state and toggle function
+  const { isDarkMode } = useDarkMode();
 
   return (
     <div className={`app-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <Navbar />
-      <div className="switch-container">
-        <label className="switch">
-          <input type="checkbox" onChange={toggleDarkMode} checked={isDarkMode} />
-          <span className="slider round"></span>
-        </label>
-        <span>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
-      </div>
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Home />} /> {/* Fallback to Home */}
+          <Route path="*" element={<Home />} /> {/* Fallback to Home for unknown routes */}
         </Routes>
       </div>
       <Footer />
