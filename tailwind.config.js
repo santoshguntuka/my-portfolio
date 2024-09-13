1559252637604f3fc60f02c0657e8a75b78b2d6c
santoshguntuka <santoshguntuka7@gmail.com>
2024-09-12 22:28:46 -0700

    Updates

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Include all JavaScript, TypeScript, JSX, and TSX files in the src directory
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
